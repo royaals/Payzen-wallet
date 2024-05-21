d2fb0d0e1f6bb931a68911fbaed091b0eed36ab8
Royal <royal.21ai040@sode-edu.in>
2024-05-21 12:25:05 +0530

    bugfix: new user initial balance creation

--- apps/frontend/app/lib/auth.ts
+++ apps/frontend/app/lib/auth.ts
@@ -1,81 +1,77 @@
 import db from "@repo/db/client";
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt";
-import { signIn, signOut } from "next-auth/react";
-
 
 export const authOptions = {
     providers: [
-   
-      CredentialsProvider({
-          name: 'Credentials',
-          credentials: {
-            name: {label: "Name", type:"text",placeholder: "Bob",required:true},
-            email: {label: "Email", type:"text",placeholder: "alex@gmail.com", required: true},
-            phone: { label: "Phone number", type: "text", placeholder: "1231231231", required: true },
-            password: { label: "Password", type: "password", required: true }
-          },
-        
-          
-       
-          async authorize(credentials: any) {
-           const hashedPassword = await bcrypt.hash(credentials.password, 10);
-            const existingUser = await db.user.findFirst({
-                where: {
-                    number: credentials.phone
-                }
-            });
-
-            if (existingUser) {
-                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
-                if (passwordValidation) {
-                    return {
-                        id: existingUser.id.toString(),
-                        name: existingUser.name,
-                        email: existingUser.number
-                    }
-                }
-                return null;
-            }
-
-            try {
-                const user = await db.user.create({
-                    data: {
-
-                        name: credentials.name,
-                        email: credentials.email,
-                        number: credentials.phone,
-                        password: hashedPassword
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                name: {label: "Name", type: "text", placeholder: "Bob", required: true},
+                email: {label: "Email", type: "text", placeholder: "alex@gmail.com", required: true},
+                phone: {label: "Phone number", type: "text", placeholder: "1231231231", required: true},
+                password: {label: "Password", type: "password", required: true}
+            },
+            async authorize(credentials: any) {
+                const hashedPassword = await bcrypt.hash(credentials.password, 10);
+                const existingUser = await db.user.findFirst({
+                    where: {
+                        number: credentials.phone
                     }
                 });
-            
-                return {
-                    id: user.id.toString(),
-                    name: user.name,
-                    email: user.number
-                }
-            } catch(e) {
-                console.error(e);
-            }
 
-            return null
-          },
+                if (existingUser) {
+                    const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                    if (passwordValidation) {
+                        return {
+                            id: existingUser.id.toString(),
+                            name: existingUser.name,
+                            email: existingUser.number
+                        }
+                    }
+                    return null;
+                }
+
+                try {
+                    const user = await db.user.create({
+                        data: {
+                            name: credentials.name,
+                            email: credentials.email,
+                            number: credentials.phone,
+                            password: hashedPassword
+                        }
+                    });
+
+                    await db.balance.create({
+                        data: {
+                            userId: user.id,
+                            amount: 0,
+                            locked: 0
+                        }
+                    });
+
+                    return {
+                        id: user.id.toString(),
+                        name: user.name,
+                        email: user.number
+                    }
+                } catch (e) {
+                    console.error(e);
+                }
+
+                return null;
+            }
         })
     ],
     pages: {
-
-    signIn: "/auth/signin",
-    signOut: "/payzen"
+        signIn: "/auth/signin",
+        signOut: "/payzen"
     },
-
     secret: process.env.JWT_SECRET || "secret",
     callbacks: {
-      
         async session({ token, session }: any) {
-            session.user.id = token.sub
-
-            return session
+            session.user.id = token.sub;
+            return session;
         }
     }
-  }
-  
+};
